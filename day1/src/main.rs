8bba88c129489ad65e8dce8233dad0d172b8226f
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-05 12:46:37 -0500

    refactor(day1): avoid creating a vector and instead accumulate the sum within the loop

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,23 +1,21 @@
 fn first_digit(s: &str) -> Option<char> {
     s.chars().find(|c| c.is_digit(10))
 }
+
 fn main() {
     let input = std::fs::read_to_string("data.txt").unwrap();
+    let mut sum: u32 = 0;
 
-    let mut acc = Vec::new();
     for line in input.lines() {
         let first = first_digit(&line).unwrap();
         let reversed: String = line.chars().rev().collect();
         let last = first_digit(&reversed).unwrap();
 
-        let mut tmp = String::new();
-        tmp.push(first);
-        tmp.push(last);
+        let combined = format!("{}{}", first, last);
+        let number: u32 = combined.parse().unwrap();
 
-        let total: u32 = tmp.parse().unwrap();
-        acc.push(total);
-        println!("{line}: {first} and {last} = {total}")
+        sum += number;
+        println!("{line}: {first} and {last} = {number}")
     }
-    let sum: u32 = acc.iter().sum();
     println!("Total: {sum}")
 }
