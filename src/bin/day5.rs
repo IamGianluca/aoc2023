4023311e90747c954f005677753807d8ab0b4670
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2023-12-19 20:23:27 -0500

    feat(day5): solve part 2, inefficient implementation

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,22 +1,26 @@
 fn main() {
-    match solve_puzzle() {
+    match solve_puzzle(&1) {
         Ok(result) => println!("Total: {result}"),
         Err(e) => eprintln!("Error: {:?}", e),
     }
 }
 
-fn solve_puzzle() -> Result<u64, Box<dyn std::error::Error>> {
+fn solve_puzzle(part: &u8) -> Result<u64, Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("day5_data.txt")?;
-    let result = solve_part1(&input)?;
+    let result = solve_part1(&input, part)?;
     Ok(result)
 }
 
-fn solve_part1(input: &str) -> Result<u64, Box<dyn std::error::Error>> {
+fn solve_part1(input: &str, part: &u8) -> Result<u64, Box<dyn std::error::Error>> {
     let first_line = input.lines().next().ok_or("could not return first line")?;
 
     // extract seed numbers from first line
     let (_, seed_str) = first_line.split_once(":").ok_or("could not find char :")?;
-    let seeds: Vec<u64> = extract_numbers(&seed_str);
+    let seeds: Vec<u64> = match part {
+        1 => Ok(extract_numbers(&seed_str)),
+        2 => Ok(extract_numbers_part2(&seed_str)),
+        &_ => Err("only handlig values 1 or 2"),
+    }?;
 
     // create an array with the names of the different mappings we will encounter
     let mapping_names = [
@@ -74,6 +78,30 @@ fn extract_numbers(text: &str) -> Vec<u64> {
         .collect()
 }
 
+fn extract_numbers_part2(text: &str) -> Vec<u64> {
+    let nums: Vec<u64> = text
+        .split_whitespace()
+        .filter_map(|num_str| num_str.parse::<u64>().ok())
+        .collect();
+
+    let mut result = Vec::new();
+    let mut last_num = 0;
+
+    for (i, num) in nums.iter().enumerate() {
+        if i % 2 != 0 {
+            // range
+            for offset in 0..*num {
+                let offset = offset as u64;
+                let something = last_num + offset;
+                result.push(something);
+            }
+        } else {
+            last_num = *num;
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod test {
     use crate::{solve_part1, solve_puzzle};
@@ -81,11 +109,22 @@ mod test {
     #[test]
     fn test_simple_part_1() {
         let input = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4";
-        assert_eq!(solve_part1(input).unwrap(), 35)
+        assert_eq!(solve_part1(input, &1).unwrap(), 35)
     }
 
     #[test]
     fn test_part_1() {
-        assert_eq!(solve_puzzle().unwrap(), 157_211_394)
+        assert_eq!(solve_puzzle(&1).unwrap(), 157_211_394)
+    }
+
+    #[test]
+    fn test_simple_part_2() {
+        let input = "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4";
+        assert_eq!(solve_part1(input, &2).unwrap(), 46)
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(solve_puzzle(&2).unwrap(), 157_211_394)
     }
 }
